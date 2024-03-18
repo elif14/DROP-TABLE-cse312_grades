dba0b8afc239ca56dce7e7da2a064906c2e2913e
Alexander Terry <alterry@buffalo.edu>
2024-03-18 15:40:35 -0400

    Got HTML and CSS to display?

--- app/app.py
+++ app/app.py
@@ -1,16 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
+from flask import send_file
 
 app = Flask(__name__)
 client = MongoClient("mongo:27017")
 
+
 @app.route('/')
 def home():
-	return "This is a homepage!"
+    return render_template('index.html')
+
+
+@app.route('/style.css')
+def send_css():
+    return send_file('/public/style.css')
+
 
 @app.route('/signup')
 def signup():
     return 'This is a sign up page!'
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
