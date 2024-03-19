124c3ce0c50c34aaea81bdc1412e0b9dfb18c8a6
Alexander Terry <alterry@buffalo.edu>
2024-03-18 22:02:15 -0400

    implemented helper function to check password validity

--- app/app.py
+++ app/app.py
@@ -3,11 +3,11 @@ from pymongo import MongoClient
 from flask import send_file
 import bcrypt
 
-
 app = Flask(__name__)
 mongo_client = MongoClient('localhost', 27017)
 db = mongo_client.flask_db
 TA_collection = db.todos
+
 
 @app.route('/')
 def home():
@@ -41,21 +41,48 @@ def sendImage():
     return response
 
 
-@app.route('/register', methods = ["POST"])
+@app.route('/register', methods=["POST"])
 def register():
     # get the form data
     username = request.form.get("username")
     password = request.form.get("password")
     confirmPassword = request.form.get("confirmPassword")
     # check if the password match
+    if not password_check(password):
+        return "password must be of length 10 with at least 1 number, lowercase letter, and uppercase letter."
     if password != confirmPassword:
-        return "passwords do not match!"
+        return "Passwords do not match."
     # salt/hash password, and store in DB
     salt = bcrypt.gensalt()
     password = bcrypt.hashpw(password.encode(), salt)
     TA_collection.insert_one({"username": username, "password": password})
     # return back to homepage
     return redirect("http://localhost:8080/", code=302)
+
+
+def password_check(password: str):
+    if len(password) < 10:
+        return False
+
+    num_appear = 0
+    upper_appear = 0
+    lower_appear = 0
+    for c in password:
+        if c in string.digits:
+            num_appear += 1
+        if c in string.ascii_uppercase:
+            upper_appear += 1
+        if c in string.ascii_lowercase:
+            lower_appear += 1
+
+    if num_appear < 1:
+        return False
+    if upper_appear < 1:
+        return False
+    if lower_appear < 1:
+        return False
+
+    return True
 
 
 if __name__ == '__main__':
