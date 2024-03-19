ac665c9e0e8158f6587aeb7070712504737b9d7f
meggi <mblam@buffalo.edu>
2024-03-18 21:55:55 -0400

    created a helper function that checks if password is secure enough

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, make_response, request, redirect
 from pymongo import MongoClient
 from flask import send_file
+import string
 
 
 app = Flask(__name__)
@@ -45,12 +46,40 @@ def register():
     # get the form data
     username = request.form.get("username")
     password = request.form.get("password")
+    # check if password is secure
+    if not password_check(password):
+        return "password must be of length 10 with at least 1 number, lowercase letter, and uppercase letter."
     confirmPassword = request.form.get("confirmPassword")
     # check if the password match
     if password != confirmPassword:
         return "passwords do not match!"
     # return back to homepage
     return redirect("http://localhost:8080/", code=302)
+
+
+def password_check(password: str):
+    if len(password) < 10:
+        return False
+
+    num_appear = 0
+    upper_appear = 0
+    lower_appear = 0
+    for c in password:
+        if c in string.digits:
+            num_appear += 1
+        if c in string.ascii_uppercase:
+            upper_appear += 1
+        if c in string.ascii_lowercase:
+            lower_appear += 1
+
+    if num_appear < 1:
+        return False
+    if upper_appear < 1:
+        return False
+    if lower_appear < 1:
+        return False
+
+    return True
 
 
 if __name__ == '__main__':
