2ae2302d4feff3d8f58b59c04750a3498c4cee0c
jespbel <jmbelaso@buffalo.edu>
2024-03-20 00:23:36 -0400

    Made a function to handle logout and creating auth token

--- register/login.py
+++ register/login.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request, redirect, current_app
 from pymongo import MongoClient
 import bcrypt
+import hashlib
+import string
+import random
 
 login_bp = Blueprint('login_bp', __name__,
     template_folder='templates',
@@ -17,11 +20,32 @@ def login():
     current_app.logger.info(username)
     current_app.logger.info(password)
     if user_exist(username) and correct_password(username, password):
-        # Jessica do your thing; set auth_token
-        return redirect('/', code=302)
+        auth_token = create_auth_token(username)
+        response = redirect('/', code=302)
+        response.set_cookie("auth_token", value=auth_token, max_age=3600, httponly=True)
+        response.headers["X-Content-Type-Options"] = "no-sniff"
+        return response
     else:
         # maybe redirect them to wrong password or error page
         return redirect('/', code=302)
+
+login_bp.route('/logout', methods=["POST"])
+def logout():
+    curr_auth = request.cookies["auth_token"]
+    new_user = ""
+    hash_obj = hashlib.sha256()
+    hash_obj.update(curr_auth.encode())
+    hash_obj.digest()
+    hash_token = hash_obj.hexdigest()
+    data_info = TA_collection.find({"auth_token": hash_token}, {"_id": 0})
+    for info in data_info:
+        # probably what's of objectID type
+        new_user = str(info["username"])
+    new_auth = create_auth_token(new_user)
+    response = redirect("/", code=302)
+    response.delete_cookie("auth_token", path="/", domain=None)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
 
 def user_exist(username: str):
     if TA_collection.find({"username": username}) is not None:
@@ -36,3 +60,16 @@ def correct_password(username, password):
         return True
     else:
         return False
+
+def create_auth_token(username):
+    auth_token = "".join(random.choices(string.ascii_letters + string.digits, k=20))
+    hash_obj = hashlib.sha256()
+    hash_obj.update(auth_token.encode)
+    hash_obj.digest()
+    needed_token = hash_obj.hexdigest()
+    filter = {"username": username}
+    update = {"$set": {"auth_token": auth_token}}
+    print(username)
+    print(auth_token)
+    TA_collection.update_one(filter, update)
+    return needed_token
