8990bfd48a35128e35079d8e55bf1c8c1c3c79df
Wonwoo Jeong <warrenww14@gmail.com>
2024-03-20 00:31:08 -0400

    Moved functions that server static homepage to homepage.py

--- register/register.py
+++ register/register.py
@@ -1,5 +1,5 @@
 import string
-from flask import Blueprint, render_template, make_response, request, redirect, send_file
+from flask import Blueprint, request, redirect
 from pymongo import MongoClient
 import bcrypt
 
@@ -13,37 +13,6 @@ db = client["cse312-project"]
 TA_collection = db['TA_collection']
 
 homepage = '/'
-
-@register_bp.route(homepage)
-def home():
-    response = render_template('index.html')
-    response = make_response(response)
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
-
-@register_bp.route('/static/style.css')
-def sendStyle():
-    response = send_file('register/static/style.css', mimetype='text/css')
-    response = make_response(response)
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
-
-@register_bp.route('/static/functions.js')
-def sendFunctions():
-    response = send_file('register/static/functions.js', mimetype='text/javascript')
-    response = make_response(response)
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
-
-
-@register_bp.route('/static/image.jpg')
-def sendImage():
-    response = send_file('register/static/image.jpg', mimetype='image/jpeg')
-    response = make_response(response)
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    return response
 
 
 @register_bp.route('/register', methods=["POST"])
