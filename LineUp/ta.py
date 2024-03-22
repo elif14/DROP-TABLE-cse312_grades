23845a8ec714b01d7f3bd6da69bb44e2c72efb6c
Wonwoo Jeong <warrenww14@gmail.com>
2024-03-22 13:30:03 -0400

    Added "ta_enqueue" and "ta_dequeue" function

--- LineUp/ta.py
+++ LineUp/ta.py
@@ -3,3 +3,13 @@ from flask import Blueprint
 ta_bp = Blueprint('ta_bp', __name__,
     template_folder='templates',
     static_folder='static')
+
+
+@ta_bp.route('/queue', methods=["POST"])
+def ta_enqueue():
+    pass
+
+
+@ta_bp.route('/dequeue', methods=["DELETE"])
+def ta_dequeue():
+    pass
