cef054c60a5ef5fd042e9052672e9cc293abdc09
jespbel <jmbelaso@buffalo.edu>
2024-03-22 14:47:59 -0400

    Changed ta.py back to it's original

--- LineUp/ta.py
+++ LineUp/ta.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file, make_response, request
+from LineUp import login
 
 ta_bp = Blueprint('ta_bp', __name__,
     template_folder='templates',
@@ -7,6 +8,18 @@ ta_bp = Blueprint('ta_bp', __name__,
 @ta_bp.route('/queue')
 def queue_page():
     response = render_template('queue.html')
+    username = "Guest"
+    if 'auth_token' in request.cookies:
+        auth_token = request.cookies.get("auth_token")
+        username = login.get_username(auth_token)
+    response = render_template('queue.html', username=username)
+    return response
+
+@ta_bp.route('/static/queue.css')
+def queue_page():
+    response = send_file('LineUp/static/queue.css', mimetype='text/css')
+    response = make_response(response)
+    response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
 
 @ta_bp.route('/queue', methods=["POST"])
