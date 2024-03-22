c565b1791d2604168ddb5d0583dc016dae104b8d
Wonwoo Jeong <warrenww14@gmail.com>
2024-03-22 13:53:10 -0400

    Added queue_page function that renders queue.html

--- LineUp/ta.py
+++ LineUp/ta.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 ta_bp = Blueprint('ta_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
+@ta_bp.route('/queue')
+def queue_page():
+    response = render_template('queue.html')
+    return response
 
 @ta_bp.route('/queue', methods=["POST"])
 def ta_enqueue():
