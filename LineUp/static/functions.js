6c16c47f5b1b11f9d8e44b8f31c603ecc31c3785
Alexander Terry <alterry@buffalo.edu>
2024-03-23 00:21:41 -0400

    Scrapped studentDisplay function

--- LineUp/static/functions.js
+++ LineUp/static/functions.js
@@ -32,10 +32,33 @@ function ta_display(){
 }
 
 function studentDisplay(){
-    const getStudentName = document.getElementById("student");
-    const studentName = getStudentName.value;
-    getStudentName.value = "";
-    const studentInfo = {"Student": studentName};
-    request.open("POST", "/student");
-    request.send(JSON.stringify(studentInfo));
+    const request = new XMLHttpRequest();
+    request.open("GET", '/student-display');
+    request.send();
+    request.onload = () => {
+        if(request.readyState == 4 && request.status == 200){
+            const response = request.response
+            let data_needed = JSON.parse(response)
+            let needed_name = ""
+            let new_arr = []
+            for (let single_char of data_needed){
+                if (single_char == ' ' || single_char == ']'){
+                    new_arr.push(needed_name)
+                    needed_name = ""
+                }
+                else if (single_char !== '['
+                    && single_char !== '"'
+                    && single_char !== ','){
+                    needed_name += single_char
+                }
+            }
+            let all_names = ""
+            for (const name of new_arr){
+                const singleName = name + " "
+                all_names += singleName
+            }
+            console.log(all_names)
+            document.getElementById("ta_names").innerHTML = all_names
+        }
+    }
 }
