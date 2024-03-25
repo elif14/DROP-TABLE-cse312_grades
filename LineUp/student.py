ba5620c4a2852df12dc911dbde3bc2680097013a
Alexander Terry <alterry@buffalo.edu>
2024-03-24 23:09:48 -0400

    Fixed date for students joining queue

--- LineUp/student.py
+++ LineUp/student.py
@@ -20,13 +20,18 @@ student_queue = db['student_queue']
 @student_bp.route('/student', methods=["GET", "POST"])
 def student_enqueue():
     if request.method == 'POST':
-        studentName = request.form.get("Name") + " " + str(datetime.datetime.now() - timedelta(days=1) + timedelta(hours=20))
-        if student_queue.find_one({"student": studentName}) is None:
-            student = {"student": studentName, "dequeued": False}
-            student_queue.insert_one(student)
+        if (request.form.get("Name").isalnum()):
+            studentName = htmlescape(request.form.get("Name")) + " " + str(datetime.datetime.now() - timedelta(days=1) + timedelta(hours=20))
+            if student_queue.find_one({"student": studentName}) is None:
+                student = {"student": studentName, "dequeued": False}
+                student_queue.insert_one(student)
         return redirect(url_for('ta_bp.queue_page'))
 
-
+def htmlescape(word):
+    word = word.replace('&', '&amp')
+    word = word.repalce('<', '&lt')
+    word = word.replace('>', '&gt')
+    return word
 
 # MEMO to Alex and Chris
 # student_queue table should have at least 2 fields
