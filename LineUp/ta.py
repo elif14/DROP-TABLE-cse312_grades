30e6af34253a96be6c75f92b4e179e6ab1502e61
zhuoquns <110157820+zhuoquns@users.noreply.github.com>
2024-04-17 22:19:49 -0400

    could not understand why it dont work. reverting back to older commit, to go straight to ngix

--- LineUp/ta.py
+++ LineUp/ta.py
@@ -100,25 +100,6 @@ def sendSocket():
 
 @socketio.on('TA-chat', namespace='/websocket')
 def TA_chat():
-    # without websockets
-    # if request.method == 'POST':
-    #     if (request.form.get("TA-chat").isalnum()):
-    #         curr_auth = request.cookies.get("auth_token")
-    #         if curr_auth is not None:
-    #             hash_obj = hashlib.sha256()
-    #             hash_obj.update(curr_auth.encode())
-    #             hash_obj.digest()
-    #             hash_token = hash_obj.hexdigest()
-    #             if TA_collection.find_one({"auth_token": hash_token}) is not None:
-    #                 print(TA_collection.find_one({"auth_token": hash_token}))
-    #                 TA_info = TA_collection.find({"auth_token": hash_token})[0]
-    #                 if TA_info is not None:
-    #                     TA_chat = {"chat": TA_info["username"] + ": " + html.escape(request.form.get("TA-chat")),
-    #                                "removed": False}
-    #                     TA_chat_collection.insert_one(TA_chat)
-    #     response = make_response(redirect(url_for('ta_bp.queue_page')))
-    #     response.headers["X-Content-Type-Options"] = "nosniff"
-    #     return response
 
     # with websockets
     chatList = []
