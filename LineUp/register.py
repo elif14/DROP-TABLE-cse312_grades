5f5b3683d26ac769919c49ba7a1369b337a53d1a
elif14 <warrenww14@gmail.com>
2024-04-20 23:01:39 +0000

    Still trying to revert back

--- LineUp/register.py
+++ LineUp/register.py
@@ -4,11 +4,9 @@ from pymongo import MongoClient
 import bcrypt
 import html
 
-
 register_bp = Blueprint('register_bp', __name__,
-    template_folder='templates',
-    static_folder='static')
-
+                        template_folder='templates',
+                        static_folder='static')
 
 client = MongoClient("mongo")
 db = client["cse312-project"]
@@ -26,24 +24,28 @@ def register():
     #     return "password must be of length 10 with at least 1 number, lowercase letter, and uppercase letter."
     if password != confirmPassword:
         return redirect('/user', code=302)
+    if username.isalnum() == False:
+        return redirect('/user', code=302)
     if TA_collection.find_one({"username": username}) is not None:
         return redirect('/user', code=302)
     # salt/hash password, and store in DB
     salt = bcrypt.gensalt()
     password = bcrypt.hashpw(password.encode(), salt)
-    user = {"username": username, 
-            "salt": salt, 
+    user = {"username": username,
+            "salt": salt,
             "hashed_password": password}
     TA_collection.insert_one(user)
     response = make_response(redirect('/user', code=302))
     response.headers["X-Content-Type-Options"] = "nosniff"
     return response
 
+
 def htmlescape(word):
     word = word.replace('&', '&amp')
     word = word.replace('<', '&lt')
     word = word.replace('>', '&gt')
     return word
+
 
 def password_check(password: str):
     if len(password) < 10:
