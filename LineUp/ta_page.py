cd27ffd9892fe0376d0b40145d55a22dfdb21274
Wonwoo Jeong <warrenww14@gmail.com>
2024-04-20 21:33:22 -0400

    Created backend for ta page

---
+++ LineUp/ta_page.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, make_response
+from pymongo import MongoClient
+
+client = MongoClient("mongo")
+db = client["cse312-project"]
+TA_collection = db['TA_collection']
+
+ta_page = Blueprint('ta_page', __name__, 
+                    template_folder = 'templates', 
+                    static_folder = 'static')
+
+
+@ta_page.route('/ta')
+def ta_page():
+    list_of_TAs = TA_collection.find({})
+
+
+    response = render_template('ta.html', list_of_TAs = list_of_TAs)
+    response = make_response(response)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
